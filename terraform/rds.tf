37f72f6dcd677662f885a562874e1d311efc32d3
Sagar Patade <sagarpatade333@gmail.com>
2026-02-19 12:34:48 +0530

    feat: use dynamic data sources and security groups

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,28 +1,28 @@
-# RDS SECURITY GROUP
-resource "aws_security_group" "rds_sg" {
-  name        = "strapi-rds-sg"
-  vpc_id      = data.aws_vpc.default.id
-  description = "Allow Strapi Tasks to access PostgreSQL"
+# 1. Create a Subnet Group using your actual subnets
+resource "aws_db_subnet_group" "strapi_db_group" {
+  name       = "strapi-db-subnet-group"
+  subnet_ids = ["subnet-03b215d73e25bf9d1", "subnet-0402e41a2030320e1"] 
 
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.strapi_sg.id]
+  tags = {
+    Name = "Strapi DB Subnet Group"
   }
 }
 
-# RDS POSTGRES INSTANCE
+# 2. Update the RDS instance to use this group
 resource "aws_db_instance" "strapi_db" {
-  allocated_storage      = 20
-  engine                 = "postgres"
-  engine_version         = "16.1"
-  instance_class         = "db.t3.micro"
-  db_name                = "strapi"
-  username               = "strapi_user"
-  password               = "SagarStrapiPass123"
-  parameter_group_name   = "default.postgres16"
-  skip_final_snapshot    = true
-  publicly_accessible    = false
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "16.1"
+  instance_class       = "db.t3.micro"
+  db_name              = "strapidb"
+  username             = "strapiuser"
+  password             = "strapipassword123"
+  parameter_group_name = "default.postgres16"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  
+  # This explicitly tells AWS which VPC/Subnets to use
+  db_subnet_group_name = aws_db_subnet_group.strapi_db_group.name
+
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 }
