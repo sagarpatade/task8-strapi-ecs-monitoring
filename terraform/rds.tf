06a471fe5e0afd04f1741ae07cd2bcba0eb212d2
Sagar Patade <sagarpatade333@gmail.com>
2026-02-19 12:48:38 +0530

    fix: declare shared data sources for subnets and vpc

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,15 +1,13 @@
-# 1. Create a Subnet Group using your actual subnets
 resource "aws_db_subnet_group" "strapi_db_group" {
-  name       = "strapi-db-subnet-group-v2"
-  # Use the first two subnet IDs to ensure we hit at least 2 AZs
-  subnet_ids = [data.aws_subnets.all.ids[0], data.aws_subnets.all.ids[1]] 
+  name       = "strapi-db-subnet-group-final"
+  # Using all discovered IDs ensures we meet the 2+ AZ requirement
+  subnet_ids = data.aws_subnets.all.ids 
 
   tags = {
     Name = "Strapi DB Subnet Group"
   }
 }
 
-# 2. Update the RDS instance to use this group
 resource "aws_db_instance" "strapi_db" {
   allocated_storage    = 20
   engine               = "postgres"
@@ -20,10 +18,7 @@ resource "aws_db_instance" "strapi_db" {
   password             = "strapipassword123"
   parameter_group_name = "default.postgres16"
   skip_final_snapshot  = true
-  publicly_accessible  = true
-  
-  # This explicitly tells AWS which VPC/Subnets to use
   db_subnet_group_name = aws_db_subnet_group.strapi_db_group.name
-
+  publicly_accessible  = true
   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 }
